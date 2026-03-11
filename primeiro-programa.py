39e85694fb166a042220c26184c3a16e39fdc2f1
RizzoKa <kauarizzo06@gmail.com>
2026-03-11 19:49:39 -0300

    variaveis

--- primeiro-programa.py
+++ primeiro-programa.py
@@ -6,3 +6,27 @@ print(True) #bool - bolleano
 
 print("Olá, mundo!") # str
 
+# no modo de execução:
+# dir() # mostra as funções e variáveis disponíveis no ambiente de execução
+# dir(100) # mostra as funções e variáveis disponíveis para o tipo int
+
+# help() # mostra a documentação das funções e variáveis disponíveis no ambiente de execução
+# help(print) # mostra a documentação da função print
+
+#As variaveis nn precisam de tipo definido 
+
+nome = "Guilherme"
+idade = 20;
+
+#eu tambem posso definir tudo em uma linha
+nome, idade = "Isadora", 27
+
+print(nome, idade)
+
+#é mais comum no python usar o nome da variável entre _ underline ( Snake Case ), do que usar tipo limiteSaqueDiario 
+limite_saque_diario = 1000
+
+# Para escrever variáveis constantes, utilizamos letras MAIUSCULAS e separação por underline
+BRAZILIAN_STATES = ["SP", "RJ", "MG", "ES"]
+
+print(BRAZILIAN_STATES)
