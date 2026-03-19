fe185d5996a5b4e042cf70b7beb46f3159379887
RizzoKa <kauarizzo06@gmail.com>
2026-03-19 14:27:18 -0300

    operadores de comparação

---
+++ operadores_comparacao.py
@@ -0,0 +1,9 @@
+saldo = 200
+saque = 200
+
+print(saldo == saque) # comparação de igualdade, retorna True 
+print(saldo != saque) # comparação de desigualdade, retorna False
+print(saldo > saque) # comparação de maior que, retorna False
+print(saldo < saque) # comparação de menor que, retorna False
+print(saldo >= saque) # comparação de maior ou igual, retorna True
+print(saldo <= saque) # comparação de menor ou igual, retorna True
