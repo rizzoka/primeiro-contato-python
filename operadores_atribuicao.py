22862fedede50b77843657fd05a91af298f526be
RizzoKa <kauarizzo06@gmail.com>
2026-03-19 14:31:51 -0300

    operadores de atribuição

---
+++ operadores_atribuicao.py
@@ -0,0 +1,22 @@
+saldo = 500 #atribuição simples
+print(saldo)
+
+saldo += 100 #atribuição de adição, equivalente a saldo = saldo + 100
+print(saldo)
+
+saldo -= 50 #atribuição de subtração, equivalente a saldo = saldo - 50
+print(saldo)
+
+saldo *= 2 #atribuição de multiplicação, equivalente a saldo = saldo * 2
+print(saldo)
+
+saldo /= 4 #atribuição de divisão, equivalente a saldo = saldo / 4
+print(saldo)
+saldo //= 2 #atribuição de divisão inteira, equivalente a saldo = saldo // 2
+print(saldo)
+
+saldo **= 3 #atribuição de potência, equivalente a saldo = saldo ** 3
+print(saldo)
+
+saldo %= 1000 #atribuição de resto da divisão, equivalente a saldo = saldo % 1000
+print(saldo)
