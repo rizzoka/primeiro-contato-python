ae82c5756efb9942997c37cd8408c3bc9a3a775a
RizzoKa <kauarizzo06@gmail.com>
2026-03-19 16:23:57 -0300

    operadores de associação

---
+++ operadores_associacao.py
@@ -0,0 +1,9 @@
+#Operadores utilizados para verificar se um objeto está presente em uma sequência 
+
+curso = 'Curso de Python'
+frutas = ["laranja", 'uva', 'limão']
+saques = [1500, 100]
+
+print("Python" in curso)  # True
+print("maçã" not in frutas)  # True
+print(200 in saques)  # False
