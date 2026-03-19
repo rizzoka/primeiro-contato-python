4ae2709e34b6f8edecccc0d4370568da508e4a66
RizzoKa <kauarizzo06@gmail.com>
2026-03-19 14:21:39 -0300

    operadores aritmeticos

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,10 @@
+print(5+2) # soma
+print(5-2) # subtração
+print(5*2) # multiplicação
+print(5/2) # divisão normal, resultado é float
+print(5//2) # divisão inteira, resultado é int, pois o // arredonda para baixo
+print(7%2) # resto da divisão, resultado é 1, pois 7 dividido por 2 tem resto 1
+print(3**4) # potência, resultado é 81, pois 3 elevado a 4 é 81
+
+x = (10 + 6) **2
+print(x)
